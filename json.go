6778c7beaf7d92c9e29172a6f68fa4a7943781f9
rafaelrmb <rafaelmotermb@gmail.com>
2024-09-05 17:30:25 -0300

    fix formatting and linter rules

--- json.go
+++ json.go
@@ -22,14 +22,14 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	dat, err := json.Marshal(payload)
 	if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-			log.Printf("Error writing response: %v", err)
-			// Note: We can't change the status code here as it's already been sent
+		log.Printf("Error writing response: %v", err)
+		// Note: We can't change the status code here as it's already been sent
 	}
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,15 +90,14 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-    Addr:              ":" + port,
-    Handler:           router,
-    ReadHeaderTimeout: 10 * time.Second,
-    ReadTimeout:       30 * time.Second,
-    WriteTimeout:      30 * time.Second,
-    IdleTimeout:       120 * time.Second,
-}
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 10 * time.Second,
+		ReadTimeout:       30 * time.Second,
+		WriteTimeout:      30 * time.Second,
+		IdleTimeout:       120 * time.Second,
+	}
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
-
 }
